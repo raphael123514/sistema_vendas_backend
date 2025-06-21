d8c36b55ce2c90dff220eb3e223c2983e11ca50b
Raphael Fonseca <raphael.fonsec@hotmail.com>
2025-06-20 23:48:16 -0300

    feat: realizando bind do kernel para funcionar o schedule

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -3,8 +3,10 @@
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
+use Illuminate\Contracts\Console\Kernel as ConsoleKernelContract;
+use App\Console\Kernel as AppConsoleKernel;
 
-return Application::configure(basePath: dirname(__DIR__))
+$app = Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
         api: __DIR__.'/../routes/api.php',
         commands: __DIR__.'/../routes/console.php',
@@ -15,4 +17,12 @@ return Application::configure(basePath: dirname(__DIR__))
     })
     ->withExceptions(function (Exceptions $exceptions): void {
         //
-    })->create();
+    })
+    ->create();
+
+// Registra o binding para o Console Kernel
+$app->singleton(ConsoleKernelContract::class, function ($app) {
+    return $app->make(AppConsoleKernel::class);
+});
+
+return $app;
